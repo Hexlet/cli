d8be2a8d2bdecc9b338b079a02e103324546868a
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-03 22:34:12 +0300

    check node version during every start
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- src/commands/program.js
+++ src/commands/program.js
@@ -1,6 +1,11 @@
 // @ts-check
 
 const _ = require('lodash');
+const semver = require('semver');
+
+if (semver.lt(process.versions.node, '14.0.0')) {
+  throw new Error('You need at least Node v14 to work with @hexlet/cli');
+}
 
 const obj = {
   command: 'program <command>',

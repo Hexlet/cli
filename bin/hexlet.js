1eee8e9b0948767bdb70ef063d951cfbff6c307e
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-08-07 21:23:48 +0300

    fix check node version

--- bin/hexlet.js
+++ bin/hexlet.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 // @ts-check
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
 const chalk = require('chalk');
 const semver = require('semver');
+
+if (semver.lt(process.versions.node, '14.0.0')) {
+  throw new Error(chalk.red('You need at least Node v14 to work with @hexlet/cli'));
+}
+
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 
 const { isRoot, checkVersion } = require('../src/utils.js');
 
 if (isRoot()) {
   throw new Error(chalk.red("Current user is root. Don't use sudo for running hexlet/cli"));
-}
-
-if (semver.lt(process.versions.node, '14.0.0')) {
-  throw new Error('You need at least Node v14 to work with @hexlet/cli');
 }
 
 checkVersion();

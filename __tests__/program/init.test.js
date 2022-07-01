f9f7c7b94408787f1143fac0e963b970ec176adb
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2022-07-01 17:46:30 +0300

    add test init program with exist config

--- __tests__/program/init.test.js
+++ __tests__/program/init.test.js
@@ -11,55 +11,71 @@ const programCmd = require('../../src/commands/program/init.js');
 const { getValidator } = require('../../src/validator/index.js');
 const { getFixturePath } = require('../helpers/index.js');
 
-nock.disableNetConnect();
+const hexletUserId = '123';
+const gitlabGroupId = '456789';
+const gitlabToken = 'some-token';
+const commandParts = ['program', 'init'];
+const args = {
+  hexletUserId, gitlabGroupId, gitlabToken, _: commandParts,
+};
+
+const project = {
+  web_url: `https://gitlab.com/hexlet/programs/ruby/groups/test-group/${hexletUserId}`,
+  http_url_to_repo: `https://gitlab.com/hexlet/programs/ruby/groups/test-group/${hexletUserId}.git`,
+  ssh_url_to_repo: `git@gitlab.com:hexlet/programs/ruby/groups/test-group/${hexletUserId}.git`,
+};
 
 describe('program', () => {
-  const scope = nock('https://gitlab.com/api/v4').persist();
   let customSettings;
-  let tmpDir;
+  let hexletDir;
 
-  beforeEach(async () => {
-    tmpDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'hexlet-cli-'));
-    customSettings = { homedir: tmpDir };
-  });
-
-  it('init', async () => {
-    const hexletUserId = '123';
-    const gitlabGroupId = '456789';
-    const gitlabToken = 'some-token';
-    const commandParts = ['program', 'init'];
-    const hexletDir = path.join(tmpDir, 'learning', 'Hexlet');
+  beforeAll(() => {
+    nock.disableNetConnect();
+    const scope = nock('https://gitlab.com/api/v4').persist();
 
     scope.get(`/namespaces/${gitlabGroupId}`)
       .reply(200, {
         full_path: 'hexlet/programs/ruby/groups/test-group',
         path: 'test-group',
       });
-
     scope.post('/projects')
       .replyWithError('Failed to create project');
-
-    const project = {
-      web_url: `https://gitlab.com/hexlet/programs/ruby/groups/test-group/${hexletUserId}`,
-      http_url_to_repo: `https://gitlab.com/hexlet/programs/ruby/groups/test-group/${hexletUserId}.git`,
-      ssh_url_to_repo: `git@gitlab.com:hexlet/programs/ruby/groups/test-group/${hexletUserId}.git`,
-    };
     scope.get(`/projects/hexlet%2Fprograms%2Fruby%2Fgroups%2Ftest-group%2F${hexletUserId}`)
       .reply(200, project);
-
     scope.post(`/projects/hexlet%2Fprograms%2Fruby%2Fgroups%2Ftest-group%2F${hexletUserId}/repository/commits`)
       .reply(200, {});
 
     git.clone = jest.fn(() => {});
     git.pull = jest.fn(() => {});
+  });
 
-    const args = {
-      hexletUserId, gitlabGroupId, gitlabToken, hexletDir, _: commandParts,
-    };
-    const result = await programCmd.handler(args, customSettings);
+  beforeEach(async () => {
+    const tmpDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'hexlet-cli-'));
+    customSettings = { homedir: tmpDir };
+    hexletDir = path.join(tmpDir, 'learning', 'Hexlet');
+  });
+
+  it('init without local config', async () => {
+    const result = await programCmd.handler({ ...args, hexletDir }, customSettings);
 
     const actualConfig = await fse.readJson(result.hexletConfigPath);
-    const expectedConfig = await fse.readJson(getFixturePath('.config.json'));
+    const expectedConfig = await fse.readJson(getFixturePath('programConfig.json'));
+    expectedConfig.hexletDir = hexletDir;
+    expect(actualConfig).toEqual(expectedConfig);
+
+    const validate = getValidator('program');
+    expect(validate(actualConfig)).toBeTruthy();
+  });
+
+  it('init with assignment config', async () => {
+    await fse.copy(
+      getFixturePath('assignmentsConfig.json'),
+      path.join(customSettings.homedir, 'Hexlet', '.config.json'),
+    );
+    const result = await programCmd.handler({ ...args, hexletDir }, customSettings);
+
+    const actualConfig = await fse.readJson(result.hexletConfigPath);
+    const expectedConfig = await fse.readJson(getFixturePath('config.json'));
     expectedConfig.hexletDir = hexletDir;
     expect(actualConfig).toEqual(expectedConfig);
 

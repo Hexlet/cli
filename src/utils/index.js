965f415d6c06979bf69c7659d4c25ebb2bedd6c3
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2022-07-05 09:36:53 +0300

    add templates for assignments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,34 @@
 // @ts-check
 
+const path = require('path');
+const fse = require('fs-extra');
 const _ = require('lodash');
+
+const git = require('./git.js');
 
 const getEntityName = (params) => {
   const commandParts = _.get(params, '_');
   return _.first(commandParts);
 };
 
+const updateTemplates = async (hexletTemplatesPath, repoPath) => {
+  const templatesPath = path.join(hexletTemplatesPath, 'assignments');
+  const templates = [
+    { fileName: 'README.md', destPath: 'README.md' },
+    { fileName: 'hexlet-check.yml', destPath: path.join('.github', 'workflows', 'hexlet-check.yml') },
+  ];
+
+  const promises = templates.map(async ({ fileName, destPath }) => {
+    await fse.copy(
+      path.join(templatesPath, fileName),
+      path.join(repoPath, destPath),
+    );
+    await git.add({ dir: repoPath, filepath: destPath });
+  });
+  await Promise.all(promises);
+};
+
 module.exports = {
   getEntityName,
+  updateTemplates,
 };
